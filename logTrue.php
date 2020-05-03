b77a3b7763a852915cf78f02adf55c19d65a354e
Barış TAŞ <baristtas@gmail.com>
2020-05-03 13:44:20 +0300

    improving readability of code

--- logTrue.php
+++ logTrue.php
@@ -1,4 +1,4 @@
-<!-- Bu sayfa login tuşuna tıklanınca açılan sayfadır -->
+<!-- Bu sayfa login submit olunca açılan sayfadır -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,17 +8,19 @@
 </head>
 <body>
 <?php
-if (isset($_POST['login']) && !empty($_POST['username']) 
-&& !empty($_POST['password'])) {
-if ($_POST['username'] == 'burak' && 
-    $_POST['password'] == '1234') {
+if (isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+if ($_POST['username'] == 'burak' && $_POST['password'] == '1234') {
     echo '<h3>Doğru bilgileri girdin ', $_POST['username'], "<br> Sekiz saniye içinde anasayfaya yönlendirilecek.</h3>";
     header( "refresh:8;url=index.html" );
+    // header 8 saniye sonra index.html'e yönlendirme yapacak
 }else {
     echo "<script>
     alert('Yanlış bilgi girildi. Tamama tıklanınca login sayfasına yönlendirilecek.');
-    window.location = 'login.php' </script>"; 
+    window.location = 'login.php' </script>";
+    // Alert bir javascript fonksiyonu. Bundan dolayı script etiketiyle yazıldı. Giriş bilgileri yanlışsa alert box açacak.
+    // Alert box'ta ok'e basınca login sayfasına geri gideceğiz.
 }
-}?>
+}
+?>
 </body>
 </html>
--- login.php
+++ login.php
@@ -1,66 +1,64 @@
 <!-- Bu sayfa login yaptığımız sayfadır -->
-<html lang = "en">
-   <head>
-      <title>Tutorialspoint.com</title>
-      <link rel="stylesheet" href="css/bootstrap.min.css">
-      <link rel="stylesheet" href="css/all.min.css">
-   </head>
-	
-   <body>
-            <!-- navbar start -->
-      <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
-         <a class="navbar-brand" href="./index.html">Burak Bağatarhan</a>
-         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-               aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-               <span class="navbar-toggler-icon"></span>
-         </button>
-         <div class="collapse navbar-collapse" id="navbarNav">
-               <ul class="navbar-nav">
-                  <li class="nav-item">
-                     <a class="nav-link" href="./index.html">Hakkımda<span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item ">
-                     <a class="nav-link" href="./ozgecmis.html">Özgeçmiş<span class="sr-only">(current)</span></a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="./sehrim.html">Sivas</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link" href="./mirasimiz.html">Mirasımız</a>
-                  </li>
-                  <li class="nav-item">
-                     <a class="nav-link " href="./iletisim.php">İletişim</a>
-                  </li>
-                  <li class="nav-item active">
-                     <a class="nav-link " href="./login.php">Giriş</a>
-                  </li>
-               </ul>
-         </div>
-      </nav>
-      <!-- navbar end -->
-      <div class="container">
-         <div class="row">
-            <div class="col text-center">
-            <h4 class="mt-4 mb-4">Kullanıcı Adınızı ve Şifreyi giriniz</h4> 
-               <form  role = "form" 
-                  action = "logTrue.php" method = "post">
-                  <input type = "text"  
-                     name = "username" placeholder = "Kullanıcı adı = burak" 
-                     required class="mb-1"></br>
-                  <input type = "password" 
-                     name = "password" placeholder = "Şifre = 1234" required class="mb-1"><br>
-                  <button class = "btn btn-primary " type = "submit" 
-                     name = "login">Login</button>
-               </form>
-            </div>
-         </div>
-      </div>
-      <div id="footer">
+<html lang="en">
 
+<head>
+   <title>Login</title>
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/all.min.css">
+</head>
+
+<body>
+   <!-- navbar start -->
+   <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+      <a class="navbar-brand" href="./index.html">Burak Bağatarhan</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+         <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+         <ul class="navbar-nav">
+            <li class="nav-item">
+               <a class="nav-link" href="./index.html">Hakkımda<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item ">
+               <a class="nav-link" href="./ozgecmis.html">Özgeçmiş<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="./sehrim.html">Sivas</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link" href="./mirasimiz.html">Mirasımız</a>
+            </li>
+            <li class="nav-item">
+               <a class="nav-link " href="./iletisim.php">İletişim</a>
+            </li>
+            <li class="nav-item active">
+               <a class="nav-link " href="./login.php">Giriş</a>
+            </li>
+         </ul>
       </div>
-      <script src="./footer.js"></script>
-      <script src="js/jquery-3.4.1.slim.min.js"></script>
-      <script src="js/popper.min.js"></script>
-      <script src="js/bootstrap.min.js"></script>
-   </body>
+   </nav>
+   <!-- navbar end -->
+   <!-- login form start -->
+   <div class="container">
+      <div class="row">
+         <div class="col text-center">
+            <h4 class="mt-4 mb-4">Kullanıcı Adınızı ve Şifreyi giriniz</h4>
+            <form role="form" action="logTrue.php" method="post">
+               <input type="text" name="username" placeholder="Kullanıcı adı = burak" class="mb-1" required ></br>
+               <input type="password" name="password" placeholder="Şifre = 1234" class="mb-1" required ><br>
+               <button class="btn btn-primary " type="submit" name="login">Login</button>
+            </form>
+         </div>
+      </div>
+   </div>
+   <!-- login form end -->
+   <!-- footer start -->
+   <div id="footer"></div>
+   <!-- footer end -->
+   <script src="./footer.js"></script>
+   <script src="js/jquery-3.4.1.slim.min.js"></script>
+   <script src="js/popper.min.js"></script>
+   <script src="js/bootstrap.min.js"></script>
+</body>
+
 </html>
