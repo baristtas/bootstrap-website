bec2b321ced2cf1eb83b02da3b99fd4491e87fe3
Barış TAŞ <baristtas@gmail.com>
2020-06-04 13:21:53 +0300

    server compability issue fix

--- logTrue.php
+++ logTrue.php
@@ -2,7 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+    <meta charset="ANSI">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login formunun yollandığı sayfa</title>
 </head>
@@ -10,8 +10,8 @@
 <?php
 if ($_POST['username'] == 'burak' && $_POST['password'] == '1234') {
     echo '<h3>Doğru bilgileri girdin ', $_POST['username'], "<br> Sekiz saniye içinde anasayfaya yönlendirilecek.</h3>";
-    header( "refresh:8;url=index.html" );
-    // header 8 saniye sonra index.html'e yönlendirme yapacak
+    echo "<script>
+    setTimeout(function () {window.location.href = 'index.html'; }, 8000); </script>";
 }else {
     echo "<script>
     alert('Yanlış bilgi girildi. Tamama tıklanınca login sayfasına yönlendirilecek.');
