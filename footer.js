8101c2c98b9fe3073ebc94bc8304bc8d0d0c7290
Barış TAŞ <baristtas@gmail.com>
2020-08-09 03:05:44 +0300

    final commit

--- footer.js
+++ footer.js
@@ -12,7 +12,6 @@ footer.innerHTML=`
       <li><a class="text-muted" href="./ozgecmis.html">Özgeçmiş</a></li>
       <li><a class="text-muted" href="./sehrim.html">Sivas</a></li>
       <li><a class="text-muted" href="./iletisim.html">İletişim</a></li>
-      <li><a class="text-muted" href="./login.php">Giriş</a></li>
     </ul> 
   </div> 
   <div class="col">
